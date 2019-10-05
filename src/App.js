04d0409cc2df36fdc4b36db8e154d5d60a2b8244
i544c <leo@i544c.me>
2019-10-06 00:22:50 +0900

    Rename Marker to CustomMarker

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Map, TileLayer } from 'react-leaflet';
-import Marker from './Marker';
+import CustomMarker from './CustomMarker';
 import './App.css';
 
 class App extends React.Component {
@@ -24,13 +24,12 @@ class App extends React.Component {
   }
 
   render() {
-    const markers = this.state.now.map(data => <Marker data={data} key={data._id} />);
+    const markers = this.state.now.map(data => <CustomMarker data={data} key={data._id} />);
 
     return (
       <div className="App">
         <Map id="map" center={[41.8327605, 140.7515623]} zoom={13}>
           <TileLayer
-            attribution='amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
           { markers }
