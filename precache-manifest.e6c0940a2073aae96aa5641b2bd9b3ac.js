efc9d7cf4c09decf462699b1a7181b830110ae48
i544c <leo@i544c.me>
2019-10-06 00:01:00 +0900

    Updates

---
+++ precache-manifest.e6c0940a2073aae96aa5641b2bd9b3ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "262a0ec151a49e02b063f87e3fd3c55e",
+    "url": "/kaiteki_jousha_front/index.html"
+  },
+  {
+    "revision": "f815f1c38d7c4e0ec982",
+    "url": "/kaiteki_jousha_front/static/css/main.f0b09313.chunk.css"
+  },
+  {
+    "revision": "c789bb67516c19252a79",
+    "url": "/kaiteki_jousha_front/static/js/2.ab9cbe0d.chunk.js"
+  },
+  {
+    "revision": "f815f1c38d7c4e0ec982",
+    "url": "/kaiteki_jousha_front/static/js/main.6d42b90c.chunk.js"
+  },
+  {
+    "revision": "7344c0cfa284e26fea0f",
+    "url": "/kaiteki_jousha_front/static/js/runtime-main.6d40e17a.js"
+  }
+]);
