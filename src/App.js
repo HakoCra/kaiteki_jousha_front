7badc672157d2812b42767916d5cea5afb90552f
i544c <leo@i544c.me>
2019-10-05 22:35:05 +0900

    Add OSM

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Map, TileLayer } from 'react-leaflet';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  async componentDidMount() {
+    this.timer = setInterval(async() => {
+      const res = await fetch('https://fukai.mybluemix.net/now');
+      const data = await res.json();
+      console.log(data);
+    }, 5000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Map id="map" center={[41.8327605, 140.7515623]} zoom={13}>
+          <TileLayer
+            attribution='amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </Map>
+      </div>
+    );
+  }
 }
 
 export default App;
